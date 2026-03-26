bb891ccd5192356da9fef6661ed4fd008ff91fd8
Rajib <rajib.somadhansoft@gmail.com>
2026-03-26 13:58:17 +0600

    Use venv inside vercel builder

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "BUILD START"
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
+python3.9 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+python manage.py collectstatic --noinput --clear
 echo "BUILD END"
