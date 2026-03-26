9f9353db4123f00f3abc60c5ea6653f6b0069e59
Rajib <rajib.somadhansoft@gmail.com>
2026-03-26 14:00:57 +0600

    Fix CRLF line endings to LF

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-echo "BUILD START"
-python3.9 -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
-python manage.py collectstatic --noinput --clear
-echo "BUILD END"
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
