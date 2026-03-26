32693c54d9b07fa92c9e655655d7ba8147e11e19
Rajib <rajib.somadhansoft@gmail.com>
2026-03-26 14:15:32 +0600

    Regenerate staticfiles with manifest and disable strict manifest mode

--- config/settings.py
+++ config/settings.py
@@ -80,6 +80,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+WHITENOISE_MANIFEST_STRICT = False
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
